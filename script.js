a485ee0a98c455d520a4da9adb1de593702da29a
Brian Simiyu <briansimiyuj@gmail.com>
2022-11-02 15:19:45 +0300

    Status codes

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const request = new XMLHttpRequest()
 
 request.addEventListener("readystatechange", () =>{
 
-    if (request.readyState === 4) {
+    if (request.readyState === 4 && request.status === 200) {
         
-        console.log(request.responseText)      // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+        console.log(request, request.responseText)   
+                                                                            
+    }else if (request.readyState === 4) {                                  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+        console.log('Could not fetch data')
 
     }
     
 }) 
 
-request.open("GET", 'https://jsonplaceholder.typicode.com/todos')
+request.open("GET", 'https://jsonplaceholder.typicode.com/todoss')
 
 request.send()
 
