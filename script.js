0f3d45c8f959ae7d733e98c6c321797cd72231d3
Brian Simiyu <briansimiyuj@gmail.com>
2022-11-08 12:08:07 +0300

    Await keyword

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const hiphopSongs = async() =>{
 
-    
+    const response = await fetch('hiphop/monaleo.json')    // Awaits stops js from assigning value to response until data is fetched
+
+    const data = await response.json()
+
+    console.log(data)
 
 }
 
 
-const music = hiphopSongs()
-
-console.log(music)
+hiphopSongs()
